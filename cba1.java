5cbbf782422c250e34607752e88bb6089c7236c7
DrKokosik <jkossak9@gmail.com>
2024-10-12 10:13:25 +0200

    's'

--- cba1.java
+++ cba1.java
@@ -1,6 +1,6 @@
-public class Main {
+public class cba1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Worlds");
     }
 
 }
